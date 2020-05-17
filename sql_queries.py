a8803612ae4219360632ee93cc02329d3a45d77a
Sophie Krüger <krueger.sophie@googlemail.com>
2020-05-17 17:01:19 +0200

    Data model improvements

--- sql_queries.py
+++ sql_queries.py
@@ -10,12 +10,12 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id varchar PRIMARY KEY, 
+    songplay_id SERIAL PRIMARY KEY, 
     start_time timestamp, 
-    user_id varchar, 
+    user_id varchar REFERENCES users (user_id), 
     level varchar, 
-    song_id varchar, 
-    artist_id varchar, 
+    song_id varchar REFERENCES songs (song_id), 
+    artist_id varchar REFERENCES artists (artist_id), 
     session_id int, 
     location varchar, 
     user_agent varchar
@@ -37,8 +37,8 @@ CREATE TABLE IF NOT EXISTS songs (
     song_id varchar PRIMARY KEY, 
     title varchar, 
     artist_id varchar,
-    year int,
-    duration numeric
+    year int NOT NULL,
+    duration float8 NOT NULL
 );
 """)
 
@@ -47,8 +47,8 @@ CREATE TABLE IF NOT EXISTS artists (
     artist_id varchar PRIMARY KEY,
     name varchar, 
     location varchar, 
-    latitude int, 
-    longitude int
+    latitude numeric, 
+    longitude numeric
 );
 """)
 
@@ -67,23 +67,27 @@ CREATE TABLE IF NOT EXISTS time (
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES (%s, %s, %s, %s, %s)
+VALUES (%s, %s, %s, %s, %s) 
+ON CONFLICT DO NOTHING
 """)
 
 time_table_insert = ("""
@@ -95,18 +99,16 @@ VALUES (%s, %s, %s, %s, %s, %s, %s)
 
 song_select = ("""
 SELECT 
-    s.artist_id
-    ,s.song_id
-    FROM songs s
-    JOIN artists art
-    ON s.artist_id = art.artist_id
-    WHERE s.title = %s
-    AND art.name = %s
-    AND s.duration = %s
-;
+songs.song_id
+,songs.artist_id
+FROM songs 
+JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title = %s
+AND artists.name = %s
+AND songs.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
